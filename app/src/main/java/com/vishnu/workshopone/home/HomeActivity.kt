614cf2c96809a8a5ab55c842cb50f32e5f4c0a7d
vishnubenny <vishnubenny@gmail.com>
2020-10-13 15:19:12 +0530

    Add navigation component in project and use in HomeActivity.kt and in activity_home.xml

--- app/src/main/java/com/vishnu/workshopone/home/HomeActivity.kt
+++ app/src/main/java/com/vishnu/workshopone/home/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.vishnu.workshopone.home
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.vishnu.core.base.BaseActivity
 import com.vishnu.workshopone.BR
 import com.vishnu.workshopone.R
@@ -15,6 +18,8 @@ class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding, HomeNaviga
 
     }
 
+    private lateinit var navigationController: NavController
+
     override fun getLayoutResId(): Int = R.layout.activity_home
 
     override fun getViewModel(): HomeViewModel = homeViewModel
@@ -27,5 +32,14 @@ class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding, HomeNaviga
         super.onCreate(savedInstanceState)
 
         homeViewModel.getUser()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navigationController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigationController.navigateUp()
     }
 }
