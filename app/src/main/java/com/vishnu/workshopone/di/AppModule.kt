a1d92ccb4b7bc38b9c27ed0f9ad91cc02724d1ef
vishnubenny <vishnubenny@gmail.com>
2020-10-13 15:13:51 +0530

    Add home module di

--- app/src/main/java/com/vishnu/workshopone/di/AppModule.kt
+++ app/src/main/java/com/vishnu/workshopone/di/AppModule.kt
@@ -1,10 +1,12 @@
 package com.vishnu.workshopone.di
 
 import com.vishnu.workshopone.di.network.networkModule
+import com.vishnu.workshopone.fraghome.di.homeFragmentModule
 import com.vishnu.workshopone.home.di.homeModule
 
 val appModule = listOf(
     networkModule,
     commonModule,
-    homeModule
+    homeModule,
+    homeFragmentModule
 )
