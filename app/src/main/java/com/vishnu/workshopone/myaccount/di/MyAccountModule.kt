d96a846e6e22b39fbbfb47d6109795b730384ed9
vishnubenny <vishnubenny@gmail.com>
2021-09-03 15:17:33 +0530

    Feature/base/task update gradle versions (#16)
    * task(Gradle Version update): Gradle Version updated Ticket-XXX
    
    * task(Gradle Version update): Gradle version, build tool version updated Ticket-XXX
    
    * task(Gradle Version update): Gradle dependency update Ticket-XXX
    
    Co-authored-by: Vishnu Benny <vishnu.benny@powerschool.com>

--- app/src/main/java/com/vishnu/workshopone/myaccount/di/MyAccountModule.kt
+++ app/src/main/java/com/vishnu/workshopone/myaccount/di/MyAccountModule.kt
@@ -2,7 +2,7 @@ package com.vishnu.workshopone.myaccount.di
 
 import com.vishnu.workshopone.myaccount.UserDataSource
 import com.vishnu.workshopone.myaccount.MyAccountViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val myAccountModule = module {
