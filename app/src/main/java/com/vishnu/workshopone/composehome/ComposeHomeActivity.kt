22999c7fa21101befde215db979e0b81d353868c
vishnubenny <vishnubenny@gmail.com>
2021-09-05 13:15:15 +0530

    task(Compose Initial setup): Added a comment Ticket-XXX

--- app/src/main/java/com/vishnu/workshopone/composehome/ComposeHomeActivity.kt
+++ app/src/main/java/com/vishnu/workshopone/composehome/ComposeHomeActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
+//Sample Compose Activity
 class ComposeHomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
